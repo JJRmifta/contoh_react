695f5a5e04a70cfb881cb26c9cb4f406c9c3029a
JJRmifta <106360945+JJRmifta@users.noreply.github.com>
2022-06-13 10:31:54 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+;import { useState } from "react"
+ 
+function App() {
+  const [title, setTitle] = useState("Hello World");
+ 
+  const changeTitle = () => {
+    setTitle("Title Changed");
+  }
+ 
+  return (
+    <div>
+      <h1>{ title }</h1>
+      <button onClick={ changeTitle }>Change Title</button>
+    </div>
+  );
+}
+ 
+export default App;
